70b0bc65d8371e1d806e0664cc130ae39756b308
Hasan <hasan.gadallah@gmail.com>
2019-07-03 10:46:51 +0300

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -36,7 +36,7 @@ var app = {
 				  }*/
 				  $(".skip_to_home")
 				  .click(function() {
-					var ref = cordova.InAppBrowser.open('https://MicroSystemEgypt.com/', '_blank', 'location=no' , 'fullscreen=yes' , 'useWideViewPort=yes' , 'zoom=no');
+					var ref = cordova.InAppBrowser.open('https://MicroSystemEgypt.com/', '_blank', 'location=no' , 'fullscreen=yes' , 'useWideViewPort=no' , 'zoom=no');
 					ref.addEventListener('loadstart', myCallback);
 				  ref.removeEventListener('loadstart', myCallback);
 
